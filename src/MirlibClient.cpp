e37113a8665a22f8c4c24e9e6534c177e197010c
compico <compico@mail.ru>
2025-06-05 20:55:53 +0300

    client fix

--- src/MirlibClient.cpp
+++ src/MirlibClient.cpp
@@ -1,18 +1,19 @@
 #include "MirlibClient.h"
+#include "MirlibDebug.h"
 
 MirlibClient::MirlibClient(uint16_t deviceAddress) : MirlibBase(deviceAddress) {
 }
 
 bool MirlibClient::sendCommand(BaseCommand *command, uint16_t targetAddress,
                                uint8_t *responseData, size_t responseSize) {
-    if (!command) {
+    if (command == nullptr) {
         setError(ERR_COMMAND_IS_NULL);
         return false;
     }
 
     // Подготовка данных запроса
     uint8_t requestData[ProtocolConstants::MAX_DATA_SIZE];
-    size_t requestDataSize = command->prepareRequest(requestData, sizeof(requestData));
+    size_t const requestDataSize = command->prepareRequest(requestData, sizeof(requestData));
 
     // Создание пакета запроса
     PacketData requestPacket;
@@ -84,85 +85,10 @@ bool MirlibClient::autoDetectGeneration(uint16_t targetAddress) {
     GetInfoCommand getInfoCmd;
 
     if (!sendCommand(&getInfoCmd, targetAddress)) {
-        return false;bool MirlibClient::sendCommand(BaseCommand *command, uint16_t targetAddress,
-                               uint8_t *responseData, size_t responseSize) {
-    if (!command) {
-        setError(ERR_COMMAND_IS_NULL);
         return false;
     }
 
-    // Подготовка данных запроса
-    uint8_t requestData[ProtocolConstants::MAX_DATA_SIZE];
-    size_t requestDataSize = command->prepareRequest(requestData, sizeof(requestData));
-
-    // Создание пакета запроса
-    PacketData requestPacket;
-    if (!ProtocolUtils::createRequestPacket(command->getCommandCode(), targetAddress,
-                                            m_deviceAddress, m_password, requestData,
-                                            requestDataSize, requestPacket)) {
-        setError(ERR_FAIL_CREATE_PACKAGE);
-        return false;
-    }
-
-#ifdef MIRLIB_DEBUG
-        debugPrintPacket(requestPacket, "Отправка запроса");
-#endif
-
-    // Отправка пакета
-    if (!sendPacketOriginalStyle(requestPacket)) {
-        setError(ERR_FAIL_SEND_PACKAGE);
-        return false;
-    }
-
-    // Ожидание ответа
-    PacketData responsePacket;
-    if (!receivePacketOriginalStyle(responsePacket, m_timeout)) {
-        setError(ERR_FAIL_RECEIVE_PACKAGE);
-        return false;
-    }
-
-#ifdef MIRLIB_DEBUG
-        debugPrintPacket(responsePacket, "Получен ответ");
-#endif
-
-    // Проверка ответа
-    if (responsePacket.command != command->getCommandCode()) {
-        setError(ERR_RESPONSE_COMMANDS_DO_NOT_MATCH);
-        return false;
-    }
-
-    if (responsePacket.srcAddress != targetAddress) {
-        setError(ERR_RESPONSE_ADDRESS_DO_NOT_MATCH);
-        return false;
-    }
-
-    if (responsePacket.destAddress != m_deviceAddress) {
-        setError(ERR_RESPONSE_TARGET_DO_NOT_MATCH);
-        return false;
-    }
-
-    if (!responsePacket.isResponse()) {
-        setError(ERR_RESPONSE_IS_NOT_RESPONSE);
-        return false;
-    }
-
-    // Разбор ответа
-    if (!command->parseResponse(responsePacket.data, responsePacket.dataSize)) {
-        setError(ERR_UNABLE_TO_PARSE_RESPONSE_DATA);
-        return false;
-    }
-
-    // Копирование данных ответа при необходимости
-    if (responseData && responseSize > 0) {
-        size_t copySize = (responsePacket.dataSize < responseSize) ? responsePacket.dataSize : responseSize;
-        memcpy(responseData, responsePacket.data, copySize);
-    }
-
-    return true;
-}
-    }
-
-    GenerationInfo info = ProtocolUtils::determineGeneration(getInfoCmd.getBoardId(), 0x32);
+    GenerationInfo const info = ProtocolUtils::determineGeneration(getInfoCmd.getBoardId(), 0x32);
     // Предполагаем role >= 0x32 для определения
 
     if (info.isOldGeneration) {
@@ -195,11 +121,11 @@ bool MirlibClient::ping(uint16_t targetAddress, uint16_t *firmwareVersion, uint1
         return false;
     }
 
-    if (firmwareVersion) {
+    if (firmwareVersion != nullptr) {
         *firmwareVersion = cmd.getFirmwareVersion();
     }
 
-    if (deviceAddress) {
+    if (deviceAddress != nullptr) {
         *deviceAddress = cmd.getDeviceAddress();
     }
 
@@ -213,7 +139,7 @@ bool MirlibClient::getInfo(uint16_t targetAddress, GetInfoResponseBase *response
         return false;
     }
 
-    if (response) {
+    if (response != nullptr) {
         *response = cmd.getResponse();
     }
 
@@ -239,8 +165,8 @@ bool MirlibClient::readStatus(uint16_t targetAddress, EnergyType energyType,
     ReadStatusCommand cmd;
 
     // Установить поколение для команды
-    GenerationInfo info = getGenerationInfo();
-    uint8_t boardId = (info.boardId != 0) ? info.boardId : 0x09; // По умолчанию новое поколение
+    GenerationInfo const info = getGenerationInfo();
+    uint8_t const boardId = (info.boardId != 0) ? info.boardId : 0x09; // По умолчанию новое поколение
     cmd.setGeneration(boardId, 0x32);
 
     cmd.setRequest(energyType);
@@ -249,11 +175,11 @@ bool MirlibClient::readStatus(uint16_t targetAddress, EnergyType energyType,
         return false;
     }
 
-    if (cmd.isOldGeneration() && oldResponse) {
+    if (cmd.isOldGeneration() && (oldResponse != nullptr)) {
         *oldResponse = cmd.getOldResponse();
     }
 
-    if (!cmd.isOldGeneration() && newResponse) {
+    if (!cmd.isOldGeneration() && (newResponse != nullptr)) {
         *newResponse = cmd.getNewResponse();
     }
 
@@ -266,8 +192,8 @@ bool MirlibClient::readInstantValue(uint16_t targetAddress, ParameterGroup group
     ReadInstantValueCommand cmd;
 
     // Установить поколение для команды
-    GenerationInfo info = getGenerationInfo();
-    uint8_t boardId = (info.boardId != 0) ? info.boardId : 0x09; // По умолчанию новое поколение
+    GenerationInfo const info = getGenerationInfo();
+    uint8_t const boardId = (info.boardId != 0) ? info.boardId : 0x09; // По умолчанию новое поколение
     cmd.setGeneration(boardId, 0x32);
 
     cmd.setRequest(group);
@@ -276,11 +202,11 @@ bool MirlibClient::readInstantValue(uint16_t targetAddress, ParameterGroup group
         return false;
     }
 
-    if (cmd.isTransitionGeneration() && transResponse) {
+    if (cmd.isTransitionGeneration() && (transResponse != nullptr)) {
         *transResponse = cmd.getTransitionResponse();
     }
 
-    if (cmd.isNewGeneration() && newResponse) {
+    if (cmd.isNewGeneration() && (newResponse != nullptr)) {
         *newResponse = cmd.getNewBasicResponse();
     }
 
